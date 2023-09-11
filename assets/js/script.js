bdf7cf284c9dc6448cb5c456c13c57dcbca9d57d
GianniMarkQuagliani <giannimarkquagliani@gmail.com>
2023-09-11 16:09:38 +0200

    [add] Ciclo for per stampare numeri da 1 a 100

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,3 +21,22 @@ Fine del ciclo
 */
 // 1) Richiamo il container per gli elementi seleziono il container tramite il suo selettore CSS.
 const container = document.querySelector('.container');
+// 2) Ciclo for per stampare numeri da 1 a 100:
+for (let i = 1; i <= 100; i++) {
+    // - Creo un elemento HTML di tipo "div"
+    const box = document.createElement('div');
+    // 3) Imposto il contenuto dell'elemento con il valore corrente del numero
+    box.textContent = i;
+    // 4) Aggiungo la classe "box" all'elemento
+    box.classList.add('box');
+    // 5) Verifico per quali numeri è divisibile e classe relativa:
+    if (!(i % 3) && !(i % 5)) {
+        box.classList.add('fizzbuzz');
+    } else if (!(i % 3)) {
+        box.classList.add('fizz');
+    } else if (!(i % 5)) {
+        box.classList.add('buzz');
+    }
+    // 6) Aggiungo l'elemento creato al container nel DOM
+    container.append(box);
+}
