c8bb780cbefa3bf33c056b75073cbfdda597b8b8
GianniMarkQuagliani <giannimarkquagliani@gmail.com>
2023-09-11 15:54:35 +0200

    [add] pseudo codice

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+/* 
+Inizio del ciclo:
+
+1) Richiamo il container per gli elementi seleziono il container tramite il suo selettore CSS.
+2) Ciclo for per stampare numeri da 1 a 100:
+3) Per ogni numero da 1 a 100:
+    - Creo di un elemento HTML e aggiunta di una classe
+    - Creo un nuovo elemento HTML di tipo "div"
+    - Imposto il contenuto dell'elemento con il valore corrente del numero
+4) Aggiungo la classe "box" all'elemento
+5) Verifico per quali numeri è divisibile e classe relativa:
+    - Se il numero è divisibile per 3 e 5: Aggiungo la classe "fizzbuzz" all'elemento.
+    - Altrimenti, se il numero è divisibile per 3: Aggiungo la classe "fizz" all'elemento.
+    - Altrimenti, se il numero è divisibile per 5: Aggiungo la classe "buzz" all'elemento
+    - Altrimenti: Continuo con l'esecuzione senza fare nulla
+        
+6) Aggiungo l'elemento creato al container nel DOM
+
+Fine del ciclo
+
+*/
